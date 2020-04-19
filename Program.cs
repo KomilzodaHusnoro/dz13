ef0baf8c6ef363ea7bc87564c28d6082457747a6
Husnoro <Husnoro>
2020-04-20 02:25:25 +0500

    добавила метод Pop, Push

--- Program.cs
+++ Program.cs
@@ -16,5 +16,19 @@ namespace _1604
             }
         }
     }
-
+    static class ArrayHelder
+    {
+        public static T Pop<T>(ref T[] array)
+        {
+            T newA = array[array.Length-1];
+            Array.Resize(ref array,array.Length-1);
+            return newA;
+        }
+        public static int Push<T> (ref T[] array, T newA)
+        {
+            Array.Resize(ref array, array.Length+1);
+            array[array.Length-1] = newA;
+            return array.Length;
+        }
+    }
 }
