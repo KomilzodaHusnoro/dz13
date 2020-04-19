018ab8d45119ebd8855d58693469130dde3bd4c7
Husnoro <Husnoro>
2020-04-20 02:34:47 +0500

    добавила Shift,  Unshift

--- Program.cs
+++ Program.cs
@@ -20,14 +20,30 @@ namespace _1604
     {
         public static T Pop<T>(ref T[] array)
         {
-            T newA = array[array.Length-1];
+            T newAE = array[array.Length-1];
             Array.Resize(ref array,array.Length-1);
-            return newA;
+            return newAE;
         }
-        public static int Push<T> (ref T[] array, T newA)
+        public static int Push<T> (ref T[] array, T newAE)
         {
             Array.Resize(ref array, array.Length+1);
-            array[array.Length-1] = newA;
+            array[array.Length-1] = newAE;
+            return array.Length;
+        }
+         public static T Shift<T> (ref T[] array)
+        {
+            T newAE = array[0];
+            Array.Reverse(array);
+            Array.Resize(ref array, array.Length-1);
+            Array.Reverse(array);
+            return newAE;
+        }
+        public static int UnShift<T>(ref T[] array, T newAE)
+        {
+            Array.Reverse(array);
+            Array.Resize(ref array,array.Length+1);
+            Array.Reverse(array);
+            array[0] = newAE;
             return array.Length;
         }
     }
