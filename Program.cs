6d6bc5ea19bc07e7772c65351ba9cad4615e8c81
Husnoro <Husnoro>
2020-04-20 02:10:36 +0500

    создала массив

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace _1604
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = {87, 76, 65, 54, 43};
+        }
+        static void ArrayAtAll <T> (T[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + " ");
+            }
         }
     }
+
 }
